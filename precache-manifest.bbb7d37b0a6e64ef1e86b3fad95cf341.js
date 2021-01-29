9a086d3f67f18025368ac8dd3f821eb32f20c8fc
Varun Herlekar <varunherlekar22@gmail.com>
2021-01-29 22:17:25 +0530

    Updates

---
+++ precache-manifest.bbb7d37b0a6e64ef1e86b3fad95cf341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c82620488c4a56949833b2cb79f1a1",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "e752e667c0986bc8a28c",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "dfb57b6a5fa316214797",
+    "url": "/box-office/static/js/2.4ec27e10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.4ec27e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e752e667c0986bc8a28c",
+    "url": "/box-office/static/js/main.e875d17e.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
